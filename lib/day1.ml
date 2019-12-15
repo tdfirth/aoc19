e7bbbb645296a5efcd4c24d5f19065fd08f618dd
Tom Firth <thomas.d.firth@gmail.com>
2019-12-15 20:31:33 +0000

    Complete part 2 of day 1

--- lib/day1.ml
+++ lib/day1.ml
@@ -1,4 +1,6 @@
-let fuel_required mass = (mass / 3) - 2
+let rec fuel_required mass =
+  let fuel = (mass / 3) - 2 in
+  if fuel < 0 then 0 else fuel + fuel_required fuel
 
 let get_weights file = List.map int_of_string (Utils.get_lines file)
 
